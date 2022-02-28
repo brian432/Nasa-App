1a45b1e98a9c8e7ff839f9fe7b71bc1814ddccfd
brian432 <brian_cabrera123@outlook.es>
2022-02-28 17:45:33 -0300

    router fix

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -13,7 +13,7 @@ const Header = ({ setState }) => {
             </div>
             <ul className={!menu ? "nav-links" : "nav-links nav-active"}>
                 {listas.map((link, index) =>
-                    <li key={index} style={menu === "" ? {} : { "animation": `navLinkFade 0.5s ease forwards ${index / 7 + .2}s` }} onClick={() => {setState(link); setMenu(""); window.scroll({ top: 0 }) }}>
+                    <li key={index} style={menu === "" ? {} : { "animation": `navLinkFade 0.5s ease forwards ${index / 7 + .2}s` }} onClick={() => {link==='sistema_solar'&& setState(link); setMenu(""); window.scroll({ top: 0 }) }}>
                         <Link to={link === 'inicio' ? '/Nasa-App/' : `/Nasa-App/${link}`} className="links hover">{link}</Link>
                     </li>
                 )}
--- src/componentes/planetas.js
+++ src/componentes/planetas.js
@@ -4,7 +4,6 @@ import { Params, useParams } from "react-router-dom";
 const Planetas = ({ state, data, setState }) => {
 
     const params = useParams();
-
     useEffect(() => {
         params.id!==undefined&&setState(params.id);
     }, [params.id])
