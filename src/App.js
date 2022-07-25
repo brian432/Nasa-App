80dee6fa723c327e5fc309a2c8a8e98b1f587c1c
brian432 <brian_cabrera123@outlook.es>
2022-07-25 17:58:52 -0300

    fixed styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './componentes/header/header'
 import { Home } from './componentes/views/home/Home';
-import {SistemaSolar} from './componentes/views/planetas/sistema-solar';
-import {Planetas} from './componentes/views/planetas/planetas';
-import {Nasa} from './componentes/views/Api-nasa/api-nasa';
+import { SistemaSolar } from './componentes/views/planetas/sistema-solar';
+import { Planetas } from './componentes/views/planetas/planetas';
+import { Nasa } from './componentes/views/Api-nasa/api-nasa';
 import { useState } from 'react'
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Header setState={setState} />
       <Routes>
         <Route path='/' element={<Home setState={setState} />} />
-        <Route path='/sistema-solar' element={<SistemaSolar  state={state} />} /> :
+        <Route path='/sistema-solar' element={<SistemaSolar state={state} />} /> :
         <Route path='/planetas/:id' element={<Planetas state={state} setState={setState} />} />
         <Route path='/api-nasa' element={<Nasa />} />
         <Route
